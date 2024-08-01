1f91916fb14f083173002e34aa39266d9c6b8464
jjeankim <miyukikj35@gmail.com>
2024-08-01 18:18:14 +0900

    Fix(html,css) & Add js

---
+++ js/login.js
@@ -0,0 +1,12 @@
+import {
+  inputEmail,
+  inputPassword,
+  checkInput,
+  emailValidation,
+  passwordValidation,
+} from "./function.js";
+
+
+
+checkInput(inputEmail, emailValidation);
+checkInput(inputPassword, passwordValidation);
