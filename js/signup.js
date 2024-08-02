6cd7766aa3f3e6f0339ac5a8875b4e671888edb2
ckx9798 <ckx12@Naver.com>
2024-08-02 21:24:48 +0900

    스프린트4

--- js/signup.js
+++ js/signup.js
@@ -1,65 +1,64 @@
 const emailInput = document.querySelector('#email')
-const passwordInput = document.querySelector('#password')
 const emailErrorbox = document.querySelector('.input-email')
+const passwordInput = document.querySelector('#password')
 const passwordErrorbox = document.querySelector('.input-password')
 const errorSpan = document.createElement('span')
-
+const pwErrorSpan = document.createElement('span')
+const nickNameErrorSpan = document.createElement('span')
+const pwcheckErrorSpan = document.createElement('span')
+const ErrorSpan = document.createElement('span')
 const loginbtn = document.querySelector('.form-btn')
-
 const nicknameInput = document.querySelector('#nickname')
 const passwordCheckInput = document.querySelector('#password-check')
 
-
-
 // 에러메시지
-function showErrorStatus(input, msg) {
+function showErrorStatus(input, span, msg) {
     input.classList.add('changeRed');
-    errorSpan.textContent = msg
-    errorSpan.classList.add('error-msg')
+    span.textContent = msg
+    span.classList.add('error-msg')
 }
 
 // 이메일
 function addErrorMessage(e) {
     if (e.target.value === "") {
-        showErrorStatus(emailInput,"이메일을 입력해주세요.");
+        showErrorStatus(emailInput, errorSpan, "이메일을 입력해주세요.");
         e.target.parentElement.appendChild(errorSpan)
-        }
-    else if (!e.target.value.includes("@")) {   
-        showErrorStatus(emailInput,"잘못된 이메일 형식입니다")
+    } else if (!e.target.value.includes("@")) {
+        showErrorStatus(emailInput, errorSpan, "잘못된 이메일 형식입니다")
         e.target.parentElement.appendChild(errorSpan)
-    }
-    else {
+    } else {
         emailInput.classList.remove('changeRed');
         errorSpan.remove(errorSpan)
     }
 }
 
+// 패스워드
 function pwErrorMessage(e) {
     if (e.target.value === "") {
-        showErrorStatus(passwordInput, "비밀번호를 입력해주세요")
-        e.target.parentElement.appendChild(errorSpan)
-        }
-    else if (e.target.value.length < 8) {
-        showErrorStatus(passwordInput, "비밀번호를 8자 이상 입력해주세요.")
-        e.target.parentElement.appendChild(errorSpan)
-    }
-    else {
+        showErrorStatus(passwordInput, pwErrorSpan, "비밀번호를 입력해주세요.")
+        e.target.parentElement.appendChild(pwErrorSpan)
+    } else if (e.target.value.length < 8) {
+        showErrorStatus(passwordInput, pwErrorSpan, "비밀번호를 8자 이상 입력해주세요.")
+        e.target.parentElement.appendChild(pwErrorSpan)
+    } else {
         passwordInput.classList.remove('changeRed');
-        errorSpan.remove(errorSpan)
-}}
-
-
-function nickNameErrorMessage(e) {
-    if (e.target.value === "") {
-        showErrorStatus(nicknameInput, "닉네임을 입력해주세요.")
-        e.target.parentElement.appendChild(errorSpan)
-        }
+        pwErrorSpan.remove(pwErrorSpan)
+    }
 }
 
+// 닉네임
+function nickNameErrorMessage(e) {
+    if (e.target.value === "") {
+        showErrorStatus(nicknameInput, nickNameErrorSpan, "닉네임을 입력해주세요.")
+        e.target.parentElement.appendChild(nickNameErrorSpan)
+    }
+}
+
+// 패스워드확인
 function pwcheckErrorMessage(e) {
     if (e.target.value !== passwordInput.value) {
-        showErrorStatus(passwordCheckInput, "비밀번호가 일치하지 않습니다.")
-        e.target.parentElement.appendChild(errorSpan)
+        showErrorStatus(passwordCheckInput, pwcheckErrorSpan, "비밀번호가 일치하지 않습니다.")
+        e.target.parentElement.appendChild(pwcheckErrorSpan)
     }
 }
 
@@ -73,4 +72,3 @@ emailInput.addEventListener('focusout', addErrorMessage)
 passwordInput.addEventListener('focusout', pwErrorMessage)
 nicknameInput.addEventListener('focusout', nickNameErrorMessage)
 passwordCheckInput.addEventListener('focusout', pwcheckErrorMessage)
-
