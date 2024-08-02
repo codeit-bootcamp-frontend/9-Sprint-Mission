ba3e57737a637260dfc17e3d39c5e13bbc4f0ec2
junjeeong <wn8624@naver.com>
2024-08-02 14:42:03 +0900

    기본 요구사항 완료

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,44 +1,56 @@
+//input 에러처리 정규표현식 
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordPattern = /^.{8,}$/;
+
 const domEmailInput = document.querySelector('#email-input');
 const domEmailWrap = document.querySelector('#email-wrap');
 const domPasswordInput = document.querySelector('#password-input');
 const domPasswordWrap = document.querySelector('#password-wrap');
-
-
+const domLoginButton = document.querySelector('.login-button');
 
 function emailErrorHandler() {
-    const imEmptyError = document.querySelector('.empty-error');
-    const imFormatError = document.querySelector('.format-error');
+    const existingEmptyError = document.querySelector('.email-empty-error');
+    const existingFormatError = document.querySelector('.email-format-error');
 
     if (!domEmailInput.value) {
-        if (imEmptyError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
-        if (imFormatError) imFormatError.remove(); //형식 에러처리를 한 상황에서 빈값 에러 메시지가 하나 더 생기는 예외처리
+        if (existingEmptyError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
+        if (existingFormatError) existingFormatError.remove(); //형식 에러처리를 한 상황에서 빈값 에러 메시지가 하나 더 생기는 예외처리
+        if (domEmailWrap.classList.remove('complete'));
 
         const el = document.createElement('div');
         domEmailInput.style.border = '1px solid #F74747';
-        el.classList.add('empty-error');
+        el.classList.add('email-empty-error');
         el.innerHTML = '이메일을 입력해 주세요.';
         domEmailWrap.appendChild(el);
     }
 
-    if (domEmailInput.value && (!domEmailInput.value.includes('@') || domEmailInput.value.endsWith('.com'))) {
-        if (imFormatError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
-        if (imEmptyError) imEmptyError.remove(); //빈값 에러처리를 한 상황에서 형식 에러 메시지가 하나 더 생기는 예외처리
-
+    if (domEmailInput.value && !emailPattern.test(domEmailInput.value)) {
+        if (existingFormatError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
+        if (existingEmptyError) existingEmptyError.remove(); //빈값 에러처리를 한 상황에서 형식 에러 메시지가 하나 더 생기는 예외처리
         const el = document.createElement('div');
         domEmailInput.style.border = '1px solid #F74747';
-        el.classList.add('format-error');
+        el.classList.add('email-format-error');
         el.innerHTML = '잘못된 이메일 형식입니다.';
         domEmailWrap.appendChild(el);
     }
+
+    if (domEmailInput.value && emailPattern.test(domEmailInput.value)) {
+        domEmailWrap.classList.add('complete');
+        if (existingEmptyError) existingEmptyError.remove();
+        if (existingFormatError) existingFormatError.remove();
+        domEmailInput.style.border = '1px solid transparent';
+    }
+
 }
 
 function passwordErrorHandler() {
-    const imEmptyError = document.querySelector('.password-empty-error');
-    const imFormatError = document.querySelector('.password-format-error')
+    const existingPasswordEmptyError = document.querySelector('.password-empty-error');
+    const existingPasswordFormatError = document.querySelector('.password-format-error');
 
     if (!domPasswordInput.value) {
-        if (imEmptyError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
-        if (imFormatError) imFormatError.remove(); //형식 에러처리를 한 상황에서 빈값 에러 메시지가 하나 더 생기는 예외처리
+        if (existingPasswordEmptyError) return;
+        if (existingPasswordFormatError) existingPasswordFormatError.remove();
+        if (domPasswordWrap.classList.remove('complete'));
 
         const el = document.createElement('div');
         domPasswordInput.style.border = '1px solid #F74747';
@@ -47,16 +59,37 @@ function passwordErrorHandler() {
         domPasswordWrap.appendChild(el);
     }
 
-    if (domPasswordInput.value.length < 8) {
-        if (imFormatError) return; //focus가 다시 되었을 때 div를 또 생성하는 예외처리
-        if (imEmptyError) imEmptyError.remove(); //형식 에러처리를 한 상황에서 빈값 에러 메시지가 하나 더 생기는 예외처리
+    if (domPasswordInput.value && !passwordPattern.test(domPasswordInput.value)) {
+        if (existingPasswordFormatError) return;
+        if (existingPasswordEmptyError) existingPasswordEmptyError.remove();
 
         const el = document.createElement('div');
-        domEmailInput.style.border = '1px solid #F74747';
+        domPasswordInput.style.border = '1px solid #F74747';
         el.classList.add('password-format-error');
-        el.innerHTML = '잘못된 이메일 형식입니다.';
+        el.innerHTML = '비밀번호를 8자 이상 입력해 주세요.';
         domPasswordWrap.appendChild(el);
     }
 
-    domEmailInput.addEventListener('blur', emailErrorHandler);
-    domPasswordInput.addEventListener('blur', passwordErrorHandler);
+    if (domPasswordInput.value && passwordPattern.test(domPasswordInput.value)) {
+        domPasswordWrap.classList.add('complete');
+        if (existingPasswordEmptyError) existingPasswordEmptyError.remove();
+        if (existingPasswordFormatError) existingPasswordFormatError.remove();
+        domPasswordInput.style.border = '1px solid transparent';
+    }
+}
+
+function activeLoinButton() {
+    if (domEmailWrap.classList.contains('complete') && domPasswordWrap.classList.contains('complete')) {
+        domLoginButton.classList.add('active');
+        domLoginButton.addEventListener('click', () => window.location.href = './items.html');
+    } else {
+        domLoginButton.classList.remove('active');
+    }
+}
+
+domEmailInput.addEventListener('blur', emailErrorHandler);
+domEmailInput.addEventListener('blur', activeLoinButton);
+
+domPasswordInput.addEventListener('blur', passwordErrorHandler);
+domPasswordInput.addEventListener('blur', activeLoinButton);
+
