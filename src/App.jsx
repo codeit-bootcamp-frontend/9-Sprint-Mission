4964267a4ad8e164efac92eb5382f813486db412
cccwon2 <cccwon2@naver.com>
2024-08-07 20:45:00 +0900

    App 불필요한 import 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import "../styles/home.css";
-import "./components/Header";
-import "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
