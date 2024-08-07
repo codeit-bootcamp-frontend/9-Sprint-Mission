08c09f284ba3fc8eb53b689ae19632c464e6873d
cccwon2 <cccwon2@naver.com>
2024-08-07 16:59:49 +0900

    메인페이지 리엑트 코드로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section id="topBanner" className="banner">
+        <div className="wrapper">
+          <h1 id="bannerTitle">
+            일상의 모든 물건을
+            <br />
+            거래해 보세요
+          </h1>
+          <a className="button pill-button" href="/items">
+            구경하러 가기
+          </a>
+        </div>
+      </section>
+      <section id="features" className="wrapper">
+        <div className="feature">
+          <img
+            className="imgHome"
+            src="images/landing/Img_home_01.png"
+            alt="인기상품"
+          />
+          <div className="feature-content">
+            <div className="feature-tag">Hot item</div>
+            <div className="feature-title">
+              인기 상품을{" "}
+              <span className="break-on-desktop">
+                <br />
+              </span>
+              확인해 보세요
+            </div>
+            <p className="feature-description">
+              가장 HOT한 중고거래 물품을
+              <br />
+              판다마켓에서 확인해 보세요
+            </p>
+          </div>
+        </div>
+        <div className="feature">
+          <img
+            className="imgHome"
+            src="images/landing/Img_home_02.png"
+            alt="검색 기능"
+          />
+          <div className="feature-content">
+            <div className="feature-tag">Search</div>
+            <div className="feature-title">
+              구매를 원하는{" "}
+              <span className="break-on-desktop">
+                <br />
+              </span>
+              상품을 검색하세요
+            </div>
+            <p className="feature-description">
+              구매하고 싶은 물품은 검색해서
+              <br />
+              쉽게 찾아보세요
+            </p>
+          </div>
+        </div>
+        <div className="feature">
+          <img
+            className="imgHome"
+            src="images/landing/Img_home_03.png"
+            alt="판매 상품 등록"
+          />
+          <div className="feature-content">
+            <div className="feature-tag">Register</div>
+            <div className="feature-title">
+              판매를 원하는{" "}
+              <span className="break-on-desktop">
+                <br />
+              </span>
+              상품을 등록하세요
+            </div>
+            <p className="feature-description">
+              어떤 물건이든 판매하고 싶은 상품을
+              <br />
+              쉽게 등록하세요
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="bottomBanner" className="banner">
+        <div className="wrapper">
+          <div className="bottom-title">
+            믿을 수 있는
+            <br />
+            판다마켓 중고거래
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
