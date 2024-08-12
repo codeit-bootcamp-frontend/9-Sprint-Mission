acd594eda527a20bdc1d231c396ce12362da4a29
Wooy <wjddndud5023@gmail.com>
2024-08-12 14:36:03 +0900

    root 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Items from "./pages/Items";
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/items" element={<Items />}></Route>
+        <Route path="/" element={<Items />}></Route>
         <Route path="/board" element={<Board />}></Route>
         <Route path="/usedMarket" element={<UsedMarket />}></Route>
         <Route path="/addItem" element={<AddItem />}></Route>
