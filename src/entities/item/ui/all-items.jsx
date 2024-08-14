7c453159c400c879a8e741a8ca725c621b342bc6
cccwon2 <cccwon2@naver.com>
2024-08-14 09:14:53 +0900

    Netlify build 오류대응5

--- src/entities/item/ui/all-items.jsx
+++ src/entities/item/ui/all-items.jsx
@@ -5,6 +5,7 @@ import useProducts from "../lib/useProducts";
 import ItemCard from "./item-card";
 import SearchIcon from "../../../shared/assets/images/icons/ic_search.svg";
 import DropDownIcon from "../../../shared/assets/images/icons/arrow_drop_down.svg";
+import DropdownList from "../../../shared/ui/dropdownlist";
 import Pagination from "../../../shared/ui/pagination";
 
 function AllItemsSection() {
@@ -50,7 +51,7 @@ function AllItemsSection() {
           </Link>
         </div>
         <div className="sortButtonWrapper">
-          {isDropdown}
+          {isDropdown && <DropdownList onSortSelection={handleSortDropdown} />}
           <button
             className="sortDropdownTriggerButton"
             onClick={handleDropdown}
