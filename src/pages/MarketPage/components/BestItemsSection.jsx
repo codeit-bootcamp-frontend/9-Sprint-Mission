ce533376ac779f4fec0691cef2ea71fcc84d591e
kim-miso <smilekim94@gmail.com>
2024-08-17 00:59:54 +0900

    Implementing best items section

--- src/pages/MarketPage/components/BestItemsSection.jsx
+++ src/pages/MarketPage/components/BestItemsSection.jsx
@@ -1,20 +1,52 @@
-import React from "react";
-import { ReactComponent as HeartIcon } from "../../../assets/icons/ic_heart.svg";
+import React, { useEffect, useState } from "react";
+import ItemCard from "./ItemCard";
+import { getProducts } from "../../../api/itemApi";
 
-function ItemCard({ item }) {
+const getPageSize = () => {
+	const width = window.innerWidth;
+	if (width < 768) {
+		return 1;
+	} else if (width < 1280) {
+		return 2;
+	} else {
+		return 4;
+	}
+};
+//이 부분은 더 효율적으로 묶어줄 수 있지 않을지 고민중
+
+function BestItemsSection() {
+	const [itemList, setItemList] = useState([]);
+	const [pageSize, setPageSize] = useState(getPageSize());
+
+	const fetchSortedData = async ({ orderBy, pageSize }) => {
+		const products = await getProducts({ orderBy, pageSize });
+		setItemList(products.list);
+	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setPageSize(getPageSize());
+		};
+
+		window.addEventListener("resize", handleResize);
+		fetchSortedData({ orderBy: "favorite", pageSize });
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, [pageSize]);
+
 	return (
-		<div className="itemCard">
-			<img src={item.images[0]} alt={item.name} className="itemCardThumbnail" />
-			<div className="itemSummary">
-				<h2 className="itemName">{item.name}</h2>
-				<p className="itemPrice">{item.price.toLocaleString()}원</p>
-				<div className="favoriteCount">
-					<HeartIcon />
-					{item.favoriteCount}
-				</div>
+		<div className="bestItemsContainer">
+			<h1 className="sectionTitle">베스트 상품</h1>
+
+			<div className="bestItemsCardSection">
+				{itemList?.map((item) => (
+					<ItemCard item={item} key={`best-item-${item.id}`} />
+				))}
 			</div>
 		</div>
 	);
 }
 
-export default ItemCard;
+export default BestItemsSection;
