c552c9edf29c7761f241b926af961c2c8d9397fb
JeongminSeon <skelters@naver.com>
2024-08-17 22:19:46 +0900

    useCallback 선언 추가

--- src/components/BestProducts.jsx
+++ src/components/BestProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { getBestProducts } from "../utils/api";
 import Product from "./Product";
 import "./BestProducts.css";
