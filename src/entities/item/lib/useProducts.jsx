1bf0f0e831b8610d89e294f357d898753d023cbb
cccwon2 <cccwon2@naver.com>
2024-08-19 13:17:59 +0900

    상품 목록을 가져오는 커스텀 훅의 외부선언 함수를 내부선언으로 변경

--- src/entities/item/lib/useProducts.jsx
+++ src/entities/item/lib/useProducts.jsx
@@ -7,28 +7,27 @@ function useProducts(page, pageSize, orderBy, keyword) {
   const [error, setError] = useState(null);
   const [totalCount, setTotalCount] = useState(0);
 
-  // fetchItems 함수는 useEffect 바깥에 선언하여 매번 생성되지 않도록...
-  const fetchItems = async ({ page, pageSize, orderBy, keyword }) => {
-    setLoading(true);
-    setError(null); // 새로운 요청이 시작될 때 에러 상태 초기화
-    try {
-      const responseInfo = await getProducts({
-        page,
-        pageSize,
-        orderBy,
-        keyword,
-      });
-      setItems(responseInfo.list);
-      setTotalCount(responseInfo.totalCount);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // useEffect에서는 의존성 배열을 주의 깊게 관리하여 불필요한 재요청을 방지
   useEffect(() => {
+    // fetchItems 를 내부 선언으로 변경
+    const fetchItems = async ({ page, pageSize, orderBy, keyword }) => {
+      setLoading(true);
+      setError(null);
+      try {
+        const responseInfo = await getProducts({
+          page,
+          pageSize,
+          orderBy,
+          keyword,
+        });
+        setItems(responseInfo.list);
+        setTotalCount(responseInfo.totalCount);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchItems({ page, pageSize, orderBy, keyword });
   }, [page, pageSize, orderBy, keyword]);
 
