08fe0048e624ed9d536415fff75793dd3c67a951
yelim hong <hongggy@gmail.com>
2024-09-13 12:48:33 +0900

    Feat : 베스트상품 pagesize 로 제어

--- src/components/App.js
+++ src/components/App.js
@@ -8,44 +8,38 @@ import throttle from "../throttle";
 import { PagenationBtn } from './Pagenation.jsx';
 
 function App() {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [pageSize, setPageSize] = useState(10); 
+  const [width, setWidth] = useState(undefined);
+  const [page, setPage] = useState(1); // 페이지네이션 버튼을 누르면 setPage()
+  const [totalPage, setTotalPage] = useState(0);
 
   //리사이즈가 발생할때마다 width 상태를 업데이트하는 핸들러 = throttle의 콜백함수
   const handleResize = () => {
     setWidth(window.innerWidth);
-  };
-  
-
-
-  const [page, setPage] = useState(1); // 페이지네이션 버튼을 누르면 setPage()
+  };  
+  useEffect(() => {
+    const throttleResizeHandler = throttle(handleResize, 500);
+    window.addEventListener("resize", throttleResizeHandler);
+    
+    return () => {
+      window.removeEventListener("resize", throttleResizeHandler);
+    };
+  }, []);
 
   //페이지네이션 컴포넌트에서 버튼 누르면 그 값을 page로 GET 요청 (AllItems 컴포넌트)
   const handlePageChange = useCallback((pageNum) => {
     setPage(pageNum);
   }, []);
 
-  const [totalPage, setTotalPage] = useState(0);
-
   const getTotalPage = (total)=>{
     setTotalPage(total);
   }
-
-  useEffect(() => {
-    const throttleResizeHandler = throttle(handleResize, 500);
-    window.addEventListener("resize", throttleResizeHandler);
-
-    return () => {
-      window.removeEventListener("resize", throttleResizeHandler);
-    };
-  }, []);
-
+  
   return (
     <>
       <Header />
       <main>
-        <BestItems width={width} page={page} pageSize={pageSize} />
-        <AllItems width={width} page={page} pageSize={pageSize} getTotalPage={getTotalPage}  />
+        <BestItems width={width} page={page}/>
+        <AllItems width={width} page={page} getTotalPage={getTotalPage}  />
         <PagenationBtn page={page} totalPage={totalPage} onPageChange={handlePageChange}/>
       </main>
     </>
