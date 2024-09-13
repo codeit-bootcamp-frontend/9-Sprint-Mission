ab56ab1b30aa59dffae2cbbda9bb072c7e331b3a
yelim hong <hongggy@gmail.com>
2024-09-13 15:56:11 +0900

    Feat : 댓글 내용 있을때 버튼 활성화

--- src/components/ReplySubmit.jsx
+++ src/components/ReplySubmit.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import styles from "./styles/ItemEachReply.module.css";
 
 export function ReplySubmit() {
+  const [content, setContent] = useState("");
+
+  const handleSubmitReply = () => {
+    // 댓글 등록 api
+    //if 댓글 등록 성공 시 인풋 초기화
+    setContent("");
+  };
+
+  const handleChangeReplyContent = (e) => {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+
   return (
     <>
       <div>
-        <form action="#" className={styles.replyForm}>
+        <form
+          action="#"
+          className={styles.replyForm}
+          onSubmit={handleSubmitReply}
+        >
           <label htmlFor="reply" className={styles.replyLabel}>
             문의하기
           </label>
           <textarea
             id="reply"
             className={styles.replyTextarea}
+            value={content}
+            onChange={handleChangeReplyContent}
             placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
           ></textarea>
-          <button className={styles.replySubmitBtn} type="submit">
+          <button
+            className={styles.replySubmitBtn}
+            disabled={content ? false : true}
+            type="submit"
+          >
             등록
           </button>
         </form>
