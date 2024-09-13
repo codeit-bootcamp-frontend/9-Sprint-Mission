0e06fb3fd9d9b13f5cadbd28bbeb10569e07aa9e
cccwon2 <cccwon2@naver.com>
2024-09-13 18:19:29 +0900

    feat: 회원가입, 로그인 다음에 accessToken, refreshToken 로컬 스토리지에 저장 feat: user/me API 추가해서 http 헤더에 Authorization: Bearer ... JWT 을 넣어 id, nickname, image 를 로컬 스토리지에 저장

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -3,14 +3,20 @@ import { SignInForm } from "../types/signin-form";
 import { SignUpForm } from "../types/signup-form";
 import { AuthResponse } from "../types/auth-response"; // 타입 임포트
 
-export const authSignIn = async (
-  formData: SignInForm
-): Promise<AuthResponse | null> => {
+export const authSignIn = async (formData: SignInForm) => {
   try {
-    const response = await ApiInstance.post<AuthResponse>(
-      "/auth/signIn",
-      formData
-    );
+    const response = await ApiInstance.post("/auth/signIn", formData);
+
+    // accessToken과 refreshToken을 localStorage에 저장
+    localStorage.setItem("accessToken", response.data.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken);
+
+    // 사용자 정보 저장 (id, nickname, image)
+    const { id, nickname, image } = response.data.user;
+    localStorage.setItem("userId", id.toString());
+    localStorage.setItem("nickname", nickname);
+    localStorage.setItem("image", image);
+
     return response.data;
   } catch (error) {
     console.error("Error logging in:", error);
@@ -19,14 +25,20 @@ export const authSignIn = async (
   }
 };
 
-export const authSignUp = async (
-  formData: SignUpForm
-): Promise<AuthResponse | null> => {
+export const authSignUp = async (formData: SignUpForm) => {
   try {
-    const response = await ApiInstance.post<AuthResponse>(
-      "/auth/signUp",
-      formData
-    );
+    const response = await ApiInstance.post("/auth/signUp", formData);
+
+    // accessToken과 refreshToken을 localStorage에 저장
+    localStorage.setItem("accessToken", response.data.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken);
+
+    // 사용자 정보 저장 (id, nickname, image)
+    const { id, nickname, image } = response.data.user;
+    localStorage.setItem("userId", id.toString());
+    localStorage.setItem("nickname", nickname);
+    localStorage.setItem("image", image);
+
     return response.data;
   } catch (error) {
     console.error("Error signing up:", error);
---
+++ src/features/user/types/user-response.d.ts
@@ -0,0 +1,7 @@
+export interface UserResponse {
+  id: number; // 유저 아이디
+  nickname: string; // 닉네임
+  image: string; // 프로필 이미지
+  createdAt: string; // 가입 일시
+  updatedAt: string; // 수정 일시
+}
