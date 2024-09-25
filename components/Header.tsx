3f8a310e87b32c800f7350822ee23ed2a708632a
yelim hong <hongggy@gmail.com>
2024-09-25 10:42:13 +0900

    Header 추가

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.menu}>
+          <Image
+            src="/logo-big.png"
+            width={153}
+            height={51}
+            alt="판다마켓"
+          ></Image>
+          <div className={styles.category}>
+            <Link href="" className={styles["category-item"]}>
+              자유게시판
+            </Link>
+            <Link href="" className={styles["category-item"]}>
+              중고마켓
+            </Link>
+          </div>
+        </div>
+        <Image src="/user.png" width={40} height={40} alt="프로필"></Image>
+      </nav>
+    </header>
+  );
+}
---
+++ pages/boards.js
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header";
+
+export default function boards() {
+  return (
+    <>
+      <div>boards 페이지 입니다.</div>
+    </>
+  );
+}
