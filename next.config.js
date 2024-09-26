80360f7c9100fac617ba10dbd11b8b697721af51
yelim hong <hongggy@gmail.com>
2024-09-26 11:52:35 +0900

    board 게시글 불러오기

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["https://panda-market-api.vercel.app/"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
