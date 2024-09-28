ba40eedf13584f6056adf472b41b7da76a142fc8
yelim hong <hongggy@gmail.com>
2024-09-29 00:42:10 +0900

    헤더 반응형

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.css";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { throttle } from "@/lib/throttle";
 
 export default function Header() {
   const router = useRouter();
   const currentPath = router.pathname;
   const isBoardsPage = currentPath.startsWith("/boards");
+
+  const initialWidth = typeof window !== "undefined" ? window.innerWidth : 1024;
+  const [width, setWidth] = useState<number>(initialWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      setWidth(newWidth);
+    };
+    const throttledResize = throttle(handleResize, 500);
+    window.addEventListener("resize", throttledResize);
+    return () => {
+      window.removeEventListener("resize", throttledResize);
+    };
+  }, []);
+
+  const logoSrc = width >= 768 ? "/logo-big.png" : "/logo-mobile.png";
+  const logoSize = width >= 768 ? [153, 51] : [81, 40]; // [width, height]
 
   return (
     <header className={styles.header}>
@@ -15,11 +34,11 @@ export default function Header() {
           {/* 홈으로 이동하는 링크 */}
           <Link href="/">
             <Image
-              src="/logo-big.png"
-              width={153}
-              height={51}
+              src={logoSrc}
+              width={logoSize[0]}
+              height={logoSize[1]}
               alt="판다마켓"
-            ></Image>
+            />
           </Link>
           <div className={styles.category}>
             <Link
