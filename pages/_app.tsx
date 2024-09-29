a47fb773bbe0b248ccc9b4e932078e8ae21b9f71
BugO <kco796@naver.com>
2024-09-29 22:17:59 +0900

    [Add]폰트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import Header from '@/src/Header';
 import GlobalStyled from '@/styles/GlobalStyled';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    as="style"
+                    crossOrigin="anonymous"
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+                />
+            </Head>
             <GlobalStyled />
             <Header />
             <Component {...pageProps} />
