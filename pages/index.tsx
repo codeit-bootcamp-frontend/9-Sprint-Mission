79d9e743e6557cf71ec41baa926edc94985993b1
yelim hong <hongggy@gmail.com>
2024-10-11 11:05:28 +0900

    메인 hero이미지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import { Hero } from "@/components/Hero";
+
 export default function index() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      <div className="container"></div>
+    </>
+  );
 }
