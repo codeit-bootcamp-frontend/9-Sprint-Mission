4521c57fb0d2b6d380f030a7c86fdbf373cbb680
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-12 00:53:12 +0900

    fix: window 에러 수정

--- hooks/usePageSize.tsx
+++ hooks/usePageSize.tsx
@@ -3,15 +3,14 @@ import useDebounce from "./useDebounce";
 
 const usePageSize = (initialPageSize: number) => {
   const [articleCount, setArticleCount] = useState(initialPageSize);
-  const [windowWidth, setWindoWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindoWidth] = useState(0);
   const debouncedWidth = useDebounce(windowWidth, 100);
 
   useEffect(() => {
     if (typeof window === "undefined") return;
 
     const handleResize = () => {
-      // 클라이언트 사이드에서만 실행
-      setWindoWidth(window.innerWidth);
+      setWindoWidth(window.innerWidth); // 클라이언트 사이드에서만 실행
       // debouncedWidth에 따라 articleCount 업데이트
       if (debouncedWidth >= 1280) {
         setArticleCount(3);
