c610acb788430fd778af7332155e7694aa65fd39
cccwon2 <cccwon2@naver.com>
2024-11-13 17:58:32 +0900

    [Feat] 개발 모드에서 react-query-devtools 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import FooterWrapper from "@/components/Layout/FooterWrapper";
 import ClientLayout from "@/components/Layout/ClientLayout";
 import "../styles/global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -29,6 +30,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <FooterWrapper />
         <div id="modal-root"></div>
       </ClientLayout>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
