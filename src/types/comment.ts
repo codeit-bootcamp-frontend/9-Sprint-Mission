06e957394b6feee211ebaa990389553ba3541821
cccwon2 <cccwon2@naver.com>
2024-11-13 17:33:51 +0900

    [Fix] 생략된 interface 추가 및 적용

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -2,3 +2,17 @@ export interface CommentListResponse {
   nextCursor: number;
   list: Comment[];
 }
+
+export interface Comment {
+  id: number;
+  content: string;
+  writer: CommentWriter;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export interface CommentWriter {
+  id: number;
+  image: string | null;
+  nickname: string | null;
+}
