5290ab66ce1d2a3ebed8eb8fc76119ec20359db8
byeolee1221 <zicsso1221@gmail.com>
2024-11-16 23:31:20 +0900

    fix: fetchComment return 값 수정

--- components/FetchComment.tsx
+++ components/FetchComment.tsx
@@ -1,6 +1,7 @@
+"use server";
+
 import { instance } from "@/lib/axios";
 import axios from "axios";
-import toast from "react-hot-toast";
 
 export const FetchComment = async (id: number, cursor: number = 0, location: string) => {
   if (!id || !location) return { list: [], nextCursor: null };
@@ -14,13 +15,12 @@ export const FetchComment = async (id: number, cursor: number = 0, location: str
     const response = await instance.get(requestUrl);
 
     if (response.status === 200) {
-      const { list, nextCursor } = response.data;
-      return { list, nextCursor };
+      return response.data;
     }
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error("댓글 조회 실패", error.response?.data);
-      toast.error(error.response?.data.message);
+      return { error: error.response?.data.message };
     }
   }
 
